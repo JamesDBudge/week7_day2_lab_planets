75a5eabb3f89067d1001842993b9cdf0221dfe95
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-05-14 15:03:12 +0100

    properly finished

--- src/views/planet_view.js
+++ src/views/planet_view.js
@@ -18,11 +18,36 @@ Planet.prototype.bindEvents = function () {
 
 Planet.prototype.render = function (planet) {
 
-  const planetName = document.createElement('h3')
+  const planetName = document.createElement('h1')
+  const planetOrbit = document.createElement('h3')
+  const planetDay = document.createElement('h3')
+  const planetArea = document.createElement('h3')
+  const planetVolume = document.createElement('h3')
+  const planetGravity = document.createElement('h3')
+  const planetMoons = document.createElement('h3')
+  const planetImage = document.createElement('IMG')
 
   planetName.textContent = `${planet.name}`
+  planetOrbit.textContent = `${planet.orbit}`
+  planetDay.textContent = `${planet.day}`
+  planetArea.textContent = `${planet.surfaceArea}`
+  planetVolume.textContent = `${planet.volume}`
+  planetGravity.textContent = `${planet.gravity}`
+  planetMoons.textContent = `${planet.moons}`
+  planetImage.src= planet.image
+
+  planetImage.setAttribute("id", "image")
+
+
   this.element.innerHTML = ' ';
   this.element.appendChild(planetName)
+  this.element.appendChild(planetOrbit)
+  this.element.appendChild(planetDay)
+  this.element.appendChild(planetArea)
+  this.element.appendChild(planetVolume)
+  this.element.appendChild(planetGravity)
+  this.element.appendChild(planetMoons)
+  this.element.appendChild(planetImage)
 
 };
 
